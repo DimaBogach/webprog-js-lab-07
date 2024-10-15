92df814569eb71ceeb7eb73ce16c9463df4923ba
Dima Bogach <dinaboga2005@gmail.com>
2024-10-15 16:06:11 +0300

    Перевірка структури файлів

---
+++ JS/task-2.js
@@ -0,0 +1,31 @@
+// Стрілочна функція для отримання користувачів з певним другом
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
+
+// Масив об'єктів користувачів для перевірки функції
+const allUsers = [
+    { name: "Moore Hensley", friends: ["Sharron Pace"] },
+    { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"] },
+    { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"] },
+    { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"] },
+    { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"] },
+    { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"] },
+    { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] },
+];
+
+// Перевірка коректності роботи функції
+console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+// Очікуваний результат:
+// [
+//   { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"] },
+//   { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// Очікуваний результат:
+// [
+//   { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"] },
+//   { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"] }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross"));
+// Очікуваний результат: []
---
+++ JS/task-3.js
@@ -0,0 +1,54 @@
+// Стрілочна функція для сортування користувачів за кількістю друзів у спадному порядку
+const sortByDescendingFriendCount = (users) => users.sort((a, b) => b.friends.length - a.friends.length);
+
+// Масив об'єктів користувачів для перевірки функції
+const usersForSorting = [
+    { name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male" },
+    { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female" },
+    { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male" },
+    { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female" },
+    { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male" },
+    { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male" },
+    { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female" },
+];
+
+// Перевірка коректності роботи функції
+console.log(sortByDescendingFriendCount(usersForSorting));
+// Очікуваний результат:
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
